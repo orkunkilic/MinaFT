c1e72bb4bdb790cbcc68194febc99a9fc87c9f21
orkunkilic <orkun.kilic@boun.edu.tr>
2022-04-18 14:57:53 +0300

    Init commit

---
+++ src/index.ts
@@ -0,0 +1,26 @@
+import { Field, SmartContract, state, State, method, UInt64 } from 'snarkyjs';
+
+/**
+ * Basic Example
+ * See https://docs.minaprotocol.com/zkapps for more info.
+ *
+ * The Add contract initializes the state variable 'num' to be a Field(1) value by default when deployed.
+ * When the 'update' method is called, the Add contract adds Field(2) to its 'num' contract state.
+ */
+export default class Add extends SmartContract {
+  @state(Field) num = State<Field>();
+
+  // initialization
+  deploy(initialBalance: UInt64, num: Field = Field(1)) {
+    super.deploy();
+    this.balance.addInPlace(initialBalance);
+    this.num.set(num);
+  }
+
+  @method async update() {
+    const currentState = await this.num.get();
+    const newState = currentState.add(2);
+    newState.assertEquals(currentState.add(2));
+    this.num.set(newState);
+  }
+}
